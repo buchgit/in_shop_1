9c15ee70af3a03450a5cdff319910458f3f36d07
buchgit <buchgit@gmail.com>
2021-03-16 00:29:59 +0300

    init. login,registration fragments

---
+++ app/src/main/java/com/example/in_shop_1/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.in_shop_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import androidx.viewpager.widget.ViewPager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
+        AuthenticationPagerAdapter pagerAdapter = new AuthenticationPagerAdapter(getSupportFragmentManager());
+        pagerAdapter.addFragment(new LoginFragment());
+        pagerAdapter.addFragment(new RegisterFragment());
+        viewPager.setAdapter(pagerAdapter);
+
+
+    }
+}
